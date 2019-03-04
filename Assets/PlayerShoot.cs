b6a24338408427ceaea829565d49c8809631a50d
Alex <43049608+ganbaKotori@users.noreply.github.com>
2019-03-03 18:39:23 -0800

    Added weapon

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+using UnityEngine.Networking;
+
+
+
+public class PlayerShoot : NetworkBehaviour
+{
+
+
+
+    private const string PLAYER_TAG = "Player";
+
+
+
+    public PlayerWeapon weapon;
+
+
+
+    [SerializeField]
+
+    private Camera cam;
+
+
+
+    [SerializeField]
+
+    private LayerMask mask;
+
+
+
+    void Start()
+
+    {
+
+        if (cam == null)
+
+        {
+
+            Debug.LogError("PlayerShoot: No camera referenced!");
+
+            this.enabled = false;
+
+        }
+
+    }
+
+
+
+    void Update()
+
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+
+        {
+
+            Shoot();
+
+        }
+
+    }
+
+
+
+
+
+    void Shoot()
+
+    {
+
+        RaycastHit _hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out _hit, weapon.range, mask))
+
+        {
+
+            Debug.Log("We hit " + _hit.collider.name);
+
+        }
+
+
+
+    }
+
+
+
+ 
+
+}
