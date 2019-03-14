8c6c7ba18b5796ad27a69c2f7c39a7d4fd08053a
Alex Ramirez <alexanderjamesramirez@gmail.com>
2019-03-14 03:18:27 -0700

    removed network manager and added better moving and jumping mechanics

---
+++ Assets/PlayerScripts/PlayerSetup.cs
@@ -0,0 +1,26 @@
+﻿//-------------------------------------
+// Responsible for setting up the player.
+
+using UnityEngine;
+
+
+[RequireComponent(typeof(Player))]
+public class PlayerSetup : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+
+        GetComponent<Player>().Setup();
+    }
+
+
+    // When we are destroyed
+    void OnDisable()
+    {
+       
+    }
+
+}
