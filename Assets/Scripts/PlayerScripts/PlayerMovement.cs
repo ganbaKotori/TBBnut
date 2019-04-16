443885a659d5d881ae97ffbfb255b9e8690b40c0
CrazyCreeper1o1 <crazysspam@gmail.com>
2019-04-15 18:31:03 -0700

    Finished the climbing mechanic and started working on an ammo system

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -8,17 +8,29 @@ public class PlayerMovement : MonoBehaviour
     public enum State { idle, moving, aiming, climbing, airborne};
     public State currentState;
 
+    //basic movement fields
     public float minimumSpeed;
     public float moveSpeed;
     public bool canClimb;
     public bool isGrounded;
+    public AnimationCurve encumbrance;
 
-    //private bool isJumping;
-    public float maxJumpVelocity;
+    //for shooting
+    public float shootingCooldown;
+    private float lastShot;
+    public GameObject acorn;
+    public Transform shootPoint;
+    public Camera fpsCam;
+    public float shootSpeed;
+    public int acornCount;
+
+    //for jumoing and air physics
+    public float maxJumpTime;
     public AnimationCurve jumpVelocity;
     private float jumpTimer;
     private float yVelocity;
 
+    //A simple class that keeps track of play inputs and direction
     public PlayerPhysics pp;
 
     private CharacterController charController;
@@ -45,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void SetState()
     {
-        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if((isGrounded || currentState.Equals(State.climbing)) && Input.GetKeyDown(KeyCode.Space))
         {
             currentState = State.airborne;
             jumpTimer = 0;
@@ -54,6 +66,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift) && canClimb)
         {
             currentState = State.climbing;
+            //isGrounded = true;
             return;
         }
         else if (!isGrounded)
@@ -78,36 +91,83 @@ public class PlayerMovement : MonoBehaviour
         
         switch (currentState)
         {
-            //If the player has no movement keys down and is on the ground, it will slow down to a stop
+               //If the player has no movement keys down and is on the ground, it will stop
             case State.idle:
-                if (pp.velocity.magnitude != 0)
-                    if (pp.velocity.magnitude >= minimumSpeed)
-                        pp.velocity = new Vector3(0, 0);
-                    else pp.velocity = new Vector3(0.8f * pp.velocity.x, 0.8f * pp.velocity.y, 0.8f * pp.velocity.z);
-                        break;
+                pp.velocity = new Vector3(0, 0);
+                break;
+
+                //Moves the player with the given inputs
             case State.moving:
                 pp.velocity = pp.playerInputs * moveSpeed;
                 charController.Move(pp.velocity * Time.deltaTime);
                 break;
+
+                //Slows the player and lets them shoot
             case State.aiming:
-                pp.velocity = pp.playerInputs * moveSpeed * 0.5f;
+                pp.velocity = pp.playerInputs * moveSpeed * 0.3f;
                 charController.Move(pp.velocity * Time.deltaTime);
+                if (Input.GetMouseButton(0))
+                    Shoot();
                 break;
+
+                //When the player is jumping and/or in the air
             case State.airborne:
                 //pp.velocity = pp.velocity + new Vector3(0, 5, 0);
                 //Debug.Log("Y: " + pp.velocity.y);
+                float terminalVelocity = -10f;
                 float newTime = jumpTimer + Time.deltaTime;
-                if(Input.GetKey(KeyCode.Space))
+                if (Input.GetKey(KeyCode.Space))
                 {
-                    yVelocity = (jumpVelocity.Evaluate(jumpTimer) + jumpVelocity.Evaluate(newTime)) / 2;
-                    Debug.Log("Timer: " + jumpTimer + "    Velovity: " + yVelocity);
+                    if (jumpTimer <= maxJumpTime)
+                    {
+                        yVelocity = (jumpVelocity.Evaluate(jumpTimer) + jumpVelocity.Evaluate(newTime)) / 2;
+                        Debug.Log("Timer: " + jumpTimer + "    Velovity: " + yVelocity);
+                        //terminalVelocity = yVelocity;
+                    }
+                    else terminalVelocity = -4f;
+                    //else yVelocity -= 3.2f * Time.deltaTime;
                 }
+                //else
+                    yVelocity = Mathf.Max(yVelocity - 9.6f * Time.deltaTime, terminalVelocity);
 
                 pp.velocity = pp.playerInputs * moveSpeed + new Vector3(0, yVelocity, 0); // * 0.5f;
                 charController.Move(pp.velocity * Time.deltaTime);
+                jumpTimer = newTime;
+
+                if (Input.GetMouseButton(0))
+                    Shoot(pp.velocity * 0.5f - fpsCam.transform.forward * 0.4f * shootSpeed);
                 break;
+
+            case State.climbing:
+                pp.velocity = (pp.playerInputs * moveSpeed) + new Vector3(0, pp.playerInputs.magnitude * moveSpeed, 0);
+                pp.velocity.Scale(new Vector3(0.3f, 0.5757f, 0.3f));
+                charController.Move(pp.velocity * Time.deltaTime);
+                yVelocity = 0;
+                break;
+
         }
     }
+    private void Shoot()
+    {
+        Shoot(new Vector3(0, 0));
+    }
+    private void Shoot(Vector3 addedVelocity)
+    {
+        if (lastShot + shootingCooldown <= Time.time && acornCount > 0)
+        {
+            acornCount--;
+
+            //Creates a projectile object
+            GameObject temp = GameObject.Instantiate(acorn, shootPoint.transform.position, new Quaternion());
+
+            //Sets the speed of the projectile and adds the velocity
+            temp.GetComponent<Rigidbody>().velocity = addedVelocity + fpsCam.transform.forward * shootSpeed;
+            lastShot = Time.time;
+        }
+
+    }
+
+
 
     private void StartJump()
     {
